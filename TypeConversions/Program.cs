ad6ca7e60311bd037b57fe99d8b2ed4461817439
DennisGabriel-Dev <dennisgabr745@gmail.com>
2026-08-06 00:31:56 -0300

    Using narrowing operation with cast in TypeConversions

--- TypeConversions/Program.cs
+++ TypeConversions/Program.cs
@@ -4,9 +4,18 @@ Console.WriteLine("***** Fun with type conversions *****");
 short numb1 = 9, numb2 = 21;
 Console.WriteLine("{0} + {1} = {2}",
   numb1, numb2, Add(numb1, numb2));
-Console.ReadLine();
 static int Add(int x, int y)
 {
   return x + y;
 }
 
+Console.WriteLine("\nUsing narrowing operation with cast.");
+
+Console.WriteLine("***** Fun with type conversions *****");
+    short numb3 = 30000, numb4 = 30000;
+    // Explicitly cast the int into a short (and allow loss of data).
+    short answer = (short)Add(numb3, numb4);
+    Console.WriteLine("{0} + {1} = {2}",
+      numb3, numb4, answer);
+    //NarrowingAttempt();
+    Console.ReadLine();
