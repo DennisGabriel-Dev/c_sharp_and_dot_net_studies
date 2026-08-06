396ae27222385c16500b12f7c934042cc6c49c4d
DennisGabriel-Dev <dennisgabr745@gmail.com>
2026-08-06 00:18:55 -0300

    add type conversions example

---
+++ TypeConversions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+Console.WriteLine("***** Fun with type conversions *****");
+// Add two shorts and print the result.
+short numb1 = 9, numb2 = 21;
+Console.WriteLine("{0} + {1} = {2}",
+  numb1, numb2, Add(numb1, numb2));
+Console.ReadLine();
+static int Add(int x, int y)
+{
+  return x + y;
+}
+
