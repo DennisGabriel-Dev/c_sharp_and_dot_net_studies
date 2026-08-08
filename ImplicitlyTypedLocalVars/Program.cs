8c20c6fd82ad485885fa505afc50e46fe7ee54fb
DennisGabriel-Dev <dennisgabr745@gmail.com>
2026-08-08 09:52:56 -0300

    add implicity type vars

---
+++ ImplicitlyTypedLocalVars/Program.cs
@@ -0,0 +1,13 @@
+﻿static void DeclareImplicitVars()
+{
+  var myInt = 0;
+  var myBool = true;
+  var myString = "C# is so good...";
+
+  Console.WriteLine("myInt is a {0}", myInt.GetType().Name);
+  Console.WriteLine("myBool is a {0}", myBool.GetType().Name);
+  Console.WriteLine("myString is a {0}", myString.GetType().Name);
+}
+
+
+DeclareImplicitVars();
