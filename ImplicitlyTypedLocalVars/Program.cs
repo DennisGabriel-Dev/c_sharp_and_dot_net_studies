ea3bd40afacd1a4b1bd402c787ab4d43cf731a0e
DennisGabriel-Dev <dennisgabr745@gmail.com>
2026-08-08 10:11:52 -0300

    implicit a lot of different kind of numbers

--- ImplicitlyTypedLocalVars/Program.cs
+++ ImplicitlyTypedLocalVars/Program.cs
@@ -1,5 +1,6 @@
 ﻿static void DeclareImplicitVars()
 {
+  Console.WriteLine("DeclareImplicitVars()");
   var myInt = 0;
   var myBool = true;
   var myString = "C# is so good...";
@@ -11,3 +12,25 @@
 
 
 DeclareImplicitVars();
+
+Console.WriteLine("-----------------------------------");
+
+static void DeclareImplicitNumerics()
+{
+  Console.WriteLine("DeclareImplicitNumerics()");
+  var myUInt = 0u;
+  var myInt = 0;
+  var myLong = 0L;
+  var myDouble = 0.0;
+  var myFloat = 0.5F;
+  var myDecimal = 0.5M;
+
+  Console.WriteLine("myUInt is a: {0}", myUInt.GetType().Name);
+  Console.WriteLine("myInt is a: {0}", myInt.GetType().Name);
+  Console.WriteLine("myLong is a: {0}", myLong.GetType().Name);
+  Console.WriteLine("myDouble is a: {0}", myDouble.GetType().Name);
+  Console.WriteLine("myFloat is a: {0}", myFloat.GetType().Name);
+  Console.WriteLine("myDecimal is a: {0}", myDecimal.GetType().Name);
+}
+
+DeclareImplicitNumerics();
