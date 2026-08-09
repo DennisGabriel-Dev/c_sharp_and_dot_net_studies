dec9bf147e1666580cbb40d5627c1bed1387c413
DennisGabriel-Dev <dennisgabr745@gmail.com>
2026-08-09 15:09:00 -0300

    add for examples part 1

---
+++ IterationsAndDecisions/Program.cs
@@ -0,0 +1,25 @@
+﻿static void ForLoopExample()
+{
+  Console.WriteLine("ForLoopExample()");
+  for (int i = 0; i < 5; i++)
+  {
+    Console.WriteLine("Number is: {0}", i);
+  }
+}
+
+ForLoopExample();
+
+Console.WriteLine("----------------------");
+
+static void ForEachExample()
+{
+  Console.WriteLine("ForEachExample()");
+  string[] cars = { "Ford", "BMW", "Yugo", "Honda" };
+
+  foreach(string car in cars)
+  {
+    Console.WriteLine(car);
+  }
+}
+
+ForEachExample();
