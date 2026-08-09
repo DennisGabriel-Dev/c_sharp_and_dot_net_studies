b888717e83c1de272ed376beffe806b0c8309f93
DennisGabriel-Dev <dennisgabr745@gmail.com>
2026-08-09 14:51:32 -0300

    add Linq query examples

--- ImplicitlyTypedLocalVars/Program.cs
+++ ImplicitlyTypedLocalVars/Program.cs
@@ -34,3 +34,25 @@ static void DeclareImplicitNumerics()
 }
 
 DeclareImplicitNumerics();
+
+Console.WriteLine("-----------------------------------");
+
+static void LinqQueryOverInts()
+{
+  Console.WriteLine("LinqQueryOverInts()");
+  int[] numbers = { 10, 20, 30, 40, 1, 2, 3, 4, 8 };
+  
+  var subset = from i in numbers where i < 10 select i;
+
+  Console.Write("Values in subset: ");
+  foreach (var num in subset)
+  {
+    Console.Write("{0}, ", num);
+  }
+  Console.WriteLine();
+
+  Console.WriteLine("subset is a: {0}", subset.GetType().Name);
+  Console.WriteLine("subset is defined in: {0}", subset.GetType().Namespace);
+}
+
+LinqQueryOverInts();
