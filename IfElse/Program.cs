a0825f484005b51924ec94ba4d8c2c7a684f4cc0
DennisGabriel-Dev <dennisgabr745@gmail.com>
2026-08-10 23:08:13 -0300

    using pattern matching "is"

--- IfElse/Program.cs
+++ IfElse/Program.cs
@@ -1,9 +1,35 @@
-﻿string myPhrase = "dennis eh lindo";
-if(myPhrase.Length > 0)
+﻿object myPhrase = "dennis eh lindo";
+
+if (myPhrase is string Abc)
 {
-  Console.WriteLine("myPhrase has a content.");
+  Console.WriteLine($"{Abc} is a {Abc.GetType().Name}");
+  if(Abc.Length > 0)
+  {
+    Console.WriteLine("myPhrase has a content.");
+  }
+  else
+  {
+    Console.WriteLine("[warning] myPhrase doesn't has a content.");
+  }
 }
-else
+
+// same result
+// if (myPhrase is string)
+// {
+//   string Abc = (string)myPhrase;
+//   Console.WriteLine($"{Abc} is a {Abc.GetType().Name}");
+//   if(Abc.Length > 0)
+//   {
+//     Console.WriteLine("myPhrase has a content.");
+//   }
+//   else
+//   {
+//     Console.WriteLine("[warning] myPhrase doesn't has a content.");
+//   }
+// }
+
+int number = 2;
+if (number is int num)
 {
-  Console.WriteLine("[warning] myPhrase doesn't has a content.");
+  Console.WriteLine($"{num} is a {num.GetType().Name}");
 }
