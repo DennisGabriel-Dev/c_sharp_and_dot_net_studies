29cb380f7e7bb51627654d1701907ec2cf879532
DennisGabriel-Dev <dennisgabr745@gmail.com>
2026-08-10 22:53:17 -0300

    add IfElse example

---
+++ IfElse/Program.cs
@@ -0,0 +1,9 @@
+﻿string myPhrase = "dennis eh lindo";
+if(myPhrase.Length > 0)
+{
+  Console.WriteLine("myPhrase has a content.");
+}
+else
+{
+  Console.WriteLine("[warning] myPhrase doesn't has a content.");
+}
