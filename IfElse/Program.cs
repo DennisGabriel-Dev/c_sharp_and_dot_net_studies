3fa8ec8e6b9a3ed368c2e4ed4bffd4d1c4f52b82
DennisGabriel-Dev <dennisgabr745@gmail.com>
2026-08-16 00:19:41 -0300

    playing with ref in C#

--- IfElse/Program.cs
+++ IfElse/Program.cs
@@ -74,6 +74,7 @@ IfElsePatternMatchingUpdatedInCSharp9();
 
 static void BuildingMyOwnMathing()
 {
+   Console.WriteLine("================ BuildingMyOwnMathing ===============/n");
   char c = '-';
   Type cType = c.GetType();
   Console.WriteLine($"Type of 'c' is {cType}");
@@ -86,3 +87,27 @@ static void BuildingMyOwnMathing()
 }
 
 BuildingMyOwnMathing();
+
+
+static void ConditionRefReference()
+{
+  Console.WriteLine("================ ConditionRefReference ===============/n");
+  int[] smallArray = new int[] { 1, 2, 3, 4, 5};
+  int[] largeArray = new int[] { 10, 20, 30, 40, 50 };
+
+  int index = 7;
+  ref int refValue = ref ((index < 5)
+   ? ref smallArray[index]
+   : ref largeArray[index - 5]);
+  
+  refValue = 0;
+
+  index = 2;
+  ((index < 5)
+    ? ref smallArray[index]
+    : ref largeArray[index - 5]) = 100;
+  Console.WriteLine(string.Join(" ", smallArray));
+  Console.WriteLine(string.Join(" ", largeArray));
+}
+
+ConditionRefReference();
